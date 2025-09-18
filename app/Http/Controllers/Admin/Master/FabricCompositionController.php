6cd1a98e4e1ed9b2a85a9072fbf1624dcdd3b39b
deepanshuIDE <deepanshu.wg07@gmail.com>
2025-09-18 15:29:04 +0530

    Add Fabric Management Features
    - Implemented FabricWidthService for managing fabric widths with CRUD operations.
    - Created views for managing fabric compositions, GSM, and weaves including create, edit, and index pages.
    - Integrated DataTables for dynamic data handling in fabric management.
    - Added form validation and error handling in the create and edit views.
    - Established routes for fabric management functionalities.

---
+++ app/Http/Controllers/Admin/Master/FabricCompositionController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Http\Controllers\Admin\Master;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\Admin\Master\FabricCompositionService as Service;
+use App\Requests\Admin\Master\FabricCompositionStoreRequest;
+use Illuminate\Support\Facades\Crypt;
+use Auth;
+
+class FabricCompositionController extends Controller { 
+    protected $service;
+    public function __construct(Service $service) {
+        $this->service = $service;
+    }
+    public function index(){
+        return view('admin.master.fabric_composition.index');
+    } 
+    public function indexList(Request $request){
+        return $this->service->indexList($request);
+    }
+    public function create(){
+        return view('admin.master.fabric_composition.create');
+    }
+    public function store(FabricCompositionStoreRequest $request){
+        $data = $this->service->store($request);
+        return redirect()->route('admin.master.fabric_composition.index')->withSuccess('The fabric composition has been successfully created.');
+    }
+    public function delete(Request $request){
+        $data = $this->service->delete($request);
+        return redirect()->route('admin.master.fabric_composition.index')->withSuccess('The fabric composition has been successfully deleted.'); 
+    }
+    public function edit(Request $request){
+        $response['data'] = $this->service->edit($request);
+        return view('admin.master.fabric_composition.edit',$response);
+    }
+    public function update(FabricCompositionStoreRequest $request){
+        $data = $this->service->update($request);
+        return redirect()->route('admin.master.fabric_composition.index')->withSuccess('The fabric composition has been successfully updated.');
+    }
+
+}
