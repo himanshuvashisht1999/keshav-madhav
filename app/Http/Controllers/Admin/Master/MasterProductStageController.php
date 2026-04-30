239adc0846467bd660a7744cc7b76adeb46b86bf
himanshuvashisht1999 <8684088727hv@gmail.com>
2026-04-30 11:18:41 +0530

    sdf

--- app/Http/Controllers/Admin/Master/MasterProductStageController.php
+++ app/Http/Controllers/Admin/Master/MasterProductStageController.php
@@ -11,52 +11,63 @@ use Auth;
 use App\Models\MasterProductStage;
 use App\Requests\Admin\Master\MasterProductSubStageStoreRequest;
 
-class MasterProductStageController extends Controller { 
+class MasterProductStageController extends Controller
+{
     protected $service;
-    public function __construct(Service $service, OrderStagesService $orderStagesService){
+    public function __construct(Service $service, OrderStagesService $orderStagesService)
+    {
         $this->orderStagesService = $orderStagesService;
         $this->service = $service;
     }
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $response['stage_data'] = $this->orderStagesService->stage_data($request);
         return view('admin.master.product_stage.index', $response);
-    } 
-    
-    public function indexList(Request $request){
+    }
+
+    public function indexList(Request $request)
+    {
         return $this->service->indexList($request);
     }
-    public function create(){
+    public function create()
+    {
         return view('admin.master.product_stage.create');
     }
-    public function store(MasterProductStageStoreRequest $request){
+    public function store(MasterProductStageStoreRequest $request)
+    {
         $data = $this->service->store($request);
         return redirect()->route('admin.master.product_stage.index')->withSuccess('The production stage has been successfully created.');
     }
-    public function delete(Request $request){
+    public function delete(Request $request)
+    {
         $data = $this->service->delete($request);
-        return redirect()->route('admin.master.product_stage.index')->withSuccess('The production stage has been successfully deleted.'); 
+        return redirect()->route('admin.master.product_stage.index')->withSuccess('The production stage has been successfully deleted.');
     }
-    public function edit(Request $request){
+    public function edit(Request $request)
+    {
         $response['data'] = $this->service->edit($request);
-        return view('admin.master.product_stage.edit',$response);
+        return view('admin.master.product_stage.edit', $response);
     }
-    public function update(MasterProductStageUpdateRequest $request){
+    public function update(MasterProductStageUpdateRequest $request)
+    {
         $data = $this->service->update($request);
         return redirect()->route('admin.master.product_stage.index')->withSuccess('The production stage has been successfully updated.');
     }
 
-    public function lotTimeIndex(Request $request){
-        $response['stages'] = MasterProductStage::orderBy('sequence', 'asc')->get();
+    public function lotTimeIndex(Request $request)
+    {
+        $response['stages'] = MasterProductStage::orderBy('sequence', 'asc')->where('status', '1')->get();
         return view('admin.master.product_stage.lot_time', $response);
     }
 
-    public function lotTimeUpdate(Request $request){
+    public function lotTimeUpdate(Request $request)
+    {
         $request->validate([
             'lot_times' => 'required|array',
             'lot_times.*' => 'required|integer|min:1',
         ]);
 
-        foreach($request->lot_times as $id => $time) {
+        foreach ($request->lot_times as $id => $time) {
             MasterProductStage::where('id', $id)->update(['lot_time_in_days' => $time]);
         }
 
@@ -64,37 +75,44 @@ class MasterProductStageController extends Controller {
     }
 
     /////// sub stage
-    public function indexSubStage(Request $request){
+    public function indexSubStage(Request $request)
+    {
         $response['stage_data'] = $this->orderStagesService->stage_data($request);
         return view('admin.master.product-sub-stage.index', $response);
-    } 
+    }
 
-    public function subStageIndex(Request $request){
+    public function subStageIndex(Request $request)
+    {
         $response['stage_data'] = $this->orderStagesService->stage_data($request);
         return view('admin.master.product_sub_stage.index', $response);
-    } 
+    }
 
-    public function subStageList(Request $request){
+    public function subStageList(Request $request)
+    {
         return $this->service->subStageList($request);
     }
-    public function editSubStage(Request $request){
+    public function editSubStage(Request $request)
+    {
         $response['data'] = $this->service->editSubStage($request);
         $sub_stage = $response['data'];
         $response['stage_data'] = MasterProductStage::find($sub_stage->master_product_stage_id);
-        return view('admin.master.product_sub_stage.edit',$response);
+        return view('admin.master.product_sub_stage.edit', $response);
     }
-    public function updateSubStage(MasterProductStageUpdateRequest $request){
+    public function updateSubStage(MasterProductStageUpdateRequest $request)
+    {
         $data = $this->service->updateSubStage($request);
         return redirect()->route('admin.master.product-sub-stage.index', ['stage_id' => $request->master_product_stage_id])->withSuccess('The stage unit has been successfully updated.');
     }
-    public function createSubStage(Request $request){
+    public function createSubStage(Request $request)
+    {
         $stage_id = $request->stage_id ?? $request->query('stage_id');
-        $response ['stage_data'] = MasterProductStage::find($stage_id);
+        $response['stage_data'] = MasterProductStage::find($stage_id);
         return view('admin.master.product_sub_stage.create', $response);
     }
-    public function storeSubStage (MasterProductSubStageStoreRequest $request){
+    public function storeSubStage(MasterProductSubStageStoreRequest $request)
+    {
         $data = $this->service->storeSubStage($request);
-        return redirect()->route('admin.master.product-sub-stage.index', ['stage_id' => $request->stage_id])-> withSuccess('The stage unit has been successfully created.');
+        return redirect()->route('admin.master.product-sub-stage.index', ['stage_id' => $request->stage_id])->withSuccess('The stage unit has been successfully created.');
     }
 
 }
