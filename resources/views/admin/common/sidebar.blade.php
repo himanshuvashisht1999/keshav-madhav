57695457ea022c829efcaf47577f31e281b89a31
himanshuvashisht1999 <8684088727hv@gmail.com>
2026-05-20 10:29:14 +0530

    Comment out Agent Ledger Report in sidebar

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -800,13 +800,13 @@ $stage_data = App\Models\MasterProductStage::orderBy('status', 'desc')->get();
                                     </a>
                                 </li>
 
-                                <li class="nav-item">
+                                {{-- <li class="nav-item">
                                     <a href="{{ route('admin.report.agent-ledger.index') }}"
                                         class="{{ str_contains(strtolower($page_url), 'admin/report/agent-ledger') ? 'nav-link active' : 'nav-link' }}">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Agent Ledger Report</p>
                                     </a>
-                                </li>
+                                </li> --}}
                             </ul>
                         </li>
                     @endcan
